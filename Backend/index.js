c568d8d606469d632f902d8b0ba6ce3c3cb424a4
Suvajit09 <suvajitroy.student@gmail.com>
2026-07-14 19:12:50 +0530

    debugging

--- Backend/index.js
+++ Backend/index.js
@@ -26,6 +26,7 @@ const corsOptions = {
 app.use(cors(
   corsOptions));
 
+console.log("Allowed Origin is:", process.env.ALLOWED_ORIGIN);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
