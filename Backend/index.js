50d309f86e6a80b4dd7d18fd135b463ce597d135
Suvajit09 <suvajitroy.student@gmail.com>
2026-07-14 19:22:05 +0530

    deleted the debugging ad fixed the allowed oriign

--- Backend/index.js
+++ Backend/index.js
@@ -15,18 +15,18 @@ import cors from 'cors';
 
 
 const port = process.env.PORT || 5000;
-const app = express();
 
 const corsOptions = {
-  origin: process.env.ALLOWED_ORIGIN || "http://localhost:3000",
+  origin: process.env.ALLOWED_ORIGIN || "http://localhost:3002",
   credentials: true,
   optionsSuccessStatus: 200
 };
+const app = express();
+
 
 app.use(cors(
   corsOptions));
 
-console.log("Allowed Origin is:", process.env.ALLOWED_ORIGIN);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
