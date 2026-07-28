849bd03c7b792df19d26ec1e4d02efc6fd0d1c12
Suvajit-99 <119094256+Suvajit09@users.noreply.github.com>
2026-07-28 22:08:11 +0530

    Update index.js dns resolution

--- Backend/index.js
+++ Backend/index.js
@@ -8,10 +8,13 @@ import nutriPlanRouter from './routes/nutritionPlan.route.js';
 import paymentRouter from './routes/payment.routes.js';
 import challengeRouter from './routes/challenge.routes.js';
 import nutriPointsRouter from './routes/nutriPoints.routes.js';
+import dns from 'dns';
 
 
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
+dns.setDefaultResultOrder('ipv4first')
 
 
 const port = process.env.PORT || 5000;
